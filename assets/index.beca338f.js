001fbff9cde27e8c9450a15ada6a611ff8993d50
RyanChent <rwqga@qq.com>
2021-04-29 18:09:22 +0800

    'fix-basic-mobile-layout'

---
+++ assets/index.beca338f.js
@@ -0,0 +1,1 @@
+import{d as e,e as a,b as n}from"./vendor.a1753273.js";const o=e({name:"VideoPage",componentName:"ManageVideoPage",setup:()=>({}),render(){const e=a("VideoPlayer");return n(e,null,null)}});export default o;
