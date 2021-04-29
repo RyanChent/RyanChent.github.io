cc9a821cd69072a0a6b5b28eec2365325d805fe0
RyanChent <rwqga@qq.com>
2021-04-29 10:30:17 +0800

    'first-commit'

---
+++ assets/index.ab9d9b9a.js
@@ -0,0 +1,1 @@
+import{d as e,e as a,b as n}from"./vendor.496c84ed.js";const o=e({name:"VideoPage",componentName:"ManageVideoPage",setup:()=>({}),render(){const e=a("VideoPlayer");return n(e,null,null)}});export default o;
---
+++ assets/index.ba53f5af.js
@@ -0,0 +1,1 @@
+import{d as e,e as a,b as n}from"./vendor.496c84ed.js";const r=e({name:"ChartsPage",componentName:"ManageChartsPage",setup(){},render(){const e=a("Echarts");return n(e,null,null)}});export default r;
