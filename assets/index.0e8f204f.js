14082f116dc22ef23d442dc01039815517952379
RyanChent <rwqga@qq.com>
2021-05-05 16:59:00 +0800

    'fix-pc-el-table'

--- assets/index.0e8f204f.js
+++
@@ -1,1 +0,0 @@
-import{d as e,e as a,b as n}from"./vendor.4c4272f9.js";const o=e({name:"VideoPage",componentName:"ManageVideoPage",setup:()=>({}),render(){const e=a("VideoPlayer");return n(e,null,null)}});export default o;
--- assets/index.18fa3f21.js
+++
@@ -1,1 +0,0 @@
-import{d as e,e as a,b as n}from"./vendor.4c4272f9.js";const r=e({name:"ChartsPage",componentName:"ManageChartsPage",setup(){},render(){const e=a("Echarts");return n(e,null,null)}});export default r;
