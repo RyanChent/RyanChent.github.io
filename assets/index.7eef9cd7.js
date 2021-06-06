f88b9148379138838f146afcac840328adcb2c63
RyanChent <rwqga@qq.com>
2021-06-06 14:08:14 +0800

    fix-some-bugs

---
+++ assets/index.7eef9cd7.js
@@ -0,0 +1,1 @@
+import{I as n,M as o}from"./index.ad94831b.js";import{d as e,l as a,c as s}from"./vendor.cd663434.js";const l=e({name:"IconPage",componentName:"ManageIconPage",components:{PCIcons:e(n),MobileIcons:e(o)},setup(){const e=a("isMobile");return()=>e.value?s(o,null,null):s(n,null,null)}});export default l;
--- assets/index.f5869f17.js
+++
@@ -1,1 +0,0 @@
-import{V as e}from"./index.b23930a6.js";import{d as o,c as n}from"./vendor.cd663434.js";const d=o({name:"VideoPage",componentName:"VideoPage",components:{VideoPlayer:o(e)},setup:()=>({}),render:()=>n(e,{wideScreen:!0,webFullScreen:!0,download:!0},null)});export default d;
