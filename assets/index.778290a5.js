0f2b9f56b2f10184ecf63844f084f6d7446c2a3e
RyanChent <rwqga@qq.com>
2021-06-12 12:10:59 +0800

    add-request-and-permission

--- assets/index.778290a5.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./index.2c2f36be.js";import{d as t,g as n,c as o,K as a}from"./vendor.a030f0a8.js";const r=t({name:"EditorPage",componentName:"ManageEditorPage",components:{Editor:t(e)},setup:()=>({content:n("")}),render(){return o(a,null,[o(e,{modelValue:this.content,"onUpdate:modelValue":e=>this.content=e},null)])}});export default r;
---
+++ assets/index.93e57909.js
@@ -0,0 +1,1 @@
+import{J as o}from"./index.1d94a869.js";import{d as a,g as r,c as e}from"./vendor.1ba13262.js";const n=a({name:"ArrayPage",componentName:"ManageArrayPage",components:{ArrayEditor:a(o)},setup:()=>({array:r(["a",function(){console.log("c")},{d:{e:[0,2],f:{1:3}}},10,()=>{console.log("h")}])}),render(){return e(o,{json:this.array,showJson:!0},null)}});export default n;
