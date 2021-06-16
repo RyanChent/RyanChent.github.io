757aa39a47c29bf9eef823724ebdb2f079082792
RyanChent <rwqga@qq.com>
2021-06-16 13:02:25 +0800

    fix-multitab-scroll

--- assets/index.586b4659.js
+++
@@ -1,1 +0,0 @@
-import{V as e}from"./index.09ff1243.js";import{d as o,c as n}from"./vendor.8de76514.js";const d=o({name:"VideoPage",componentName:"VideoPage",components:{VideoPlayer:o(e)},render:()=>n(e,{wideScreen:!0,webFullScreen:!0,download:!0},null)});export default d;
---
+++ assets/index.6a46e4e6.js
@@ -0,0 +1,1 @@
+import{V as e}from"./index.753c2d8d.js";import{d as o,c as a}from"./vendor.5f125aaa.js";const d=o({name:"VideoPage",componentName:"VideoPage",components:{VideoPlayer:o(e)},render:()=>a(e,{wideScreen:!0,webFullScreen:!0,download:!0},null)});export default d;
