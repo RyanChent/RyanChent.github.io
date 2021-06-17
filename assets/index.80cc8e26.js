9bf95bb93869ce7302d79efff0cb05462fba04ef
RyanChent <rwqga@qq.com>
2021-06-17 15:31:11 +0800

    add-sitemap.xml

---
+++ assets/index.80cc8e26.js
@@ -0,0 +1,1 @@
+import{V as e}from"./index.e147440b.js";import{d as o,c as a}from"./vendor.5f125aaa.js";const n=o({name:"VideoPage",componentName:"VideoPage",components:{VideoPlayer:o(e)},render:()=>a(e,{wideScreen:!0,webFullScreen:!0,download:!0},null)});export default n;
