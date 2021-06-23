7fcbe6353d3184b2a4d2076d55825a0c198af78f
RyanChent <rwqga@qq.com>
2021-06-23 23:53:20 +0800

    add-register-form

---
+++ assets/index.7c6ba4d4.js
@@ -0,0 +1,1 @@
+import{V as e}from"./index.91574766.js";import{d as o,c as n}from"./vendor.0c535a2d.js";const d=o({name:"VideoPage",componentName:"VideoPage",components:{VideoPlayer:o(e)},render:()=>n(e,{wideScreen:!0,webFullScreen:!0,download:!0},null)});export default d;
--- assets/index.faef894b.js
+++
@@ -1,1 +0,0 @@
-import{V as e}from"./index.c122fe14.js";import{d as o,c as n}from"./vendor.0c535a2d.js";const d=o({name:"VideoPage",componentName:"VideoPage",components:{VideoPlayer:o(e)},render:()=>n(e,{wideScreen:!0,webFullScreen:!0,download:!0},null)});export default d;
