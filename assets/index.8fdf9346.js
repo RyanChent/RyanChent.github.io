09a47eeaff0b48fcfa9616a2b37a7d1acfc153a7
RyanChent <rwqga@qq.com>
2021-06-24 18:03:31 +0800

    add-some-register-forget

--- assets/index.8fdf9346.js
+++
@@ -1,1 +0,0 @@
-import{V as e}from"./index.c824c07e.js";import{d as o,c as d}from"./vendor.1d1fbd97.js";const n=o({name:"VideoPage",componentName:"VideoPage",components:{VideoPlayer:o(e)},render:()=>d(e,{wideScreen:!0,webFullScreen:!0,download:!0},null)});export default n;
