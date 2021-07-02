21dde81bd781cde5fe2849f768faa509f543ba37
RyanChent <rwqga@qq.com>
2021-07-02 11:38:17 +0800

    add-function-contextmenu

--- assets/index.692b7d63.js
+++
@@ -1,1 +0,0 @@
-import{V as e}from"./index.76deb057.js";import{d as o,c as d}from"./vendor.cbcd8de1.js";const n=o({name:"VideoPage",componentName:"VideoPage",components:{VideoPlayer:o(e)},render:()=>d(e,{wideScreen:!0,webFullScreen:!0,download:!0},null)});export default n;
---
+++ assets/index.9d90043d.js
@@ -0,0 +1,1 @@
+import{V as e}from"./index.6f439a15.js";import{d as o,c as d}from"./vendor.cbcd8de1.js";const n=o({name:"VideoPage",componentName:"VideoPage",components:{VideoPlayer:o(e)},render:()=>d(e,{wideScreen:!0,webFullScreen:!0,download:!0},null)});export default n;
