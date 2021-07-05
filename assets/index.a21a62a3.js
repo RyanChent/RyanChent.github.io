8f54affed27e88b3676190a7d2ca4ce19237d005
RyanChent <rwqga@qq.com>
2021-07-05 23:28:12 +0800

    rest-toolbar-drag

---
+++ assets/index.a21a62a3.js
@@ -0,0 +1,1 @@
+import{I as n,M as o}from"./index.ddccdc8b.js";import{d as e,v as s,c as a}from"./vendor.7bcbf4d5.js";const c=e({name:"IconPage",componentName:"ManageIconPage",components:{PCIcons:e(n),MobileIcons:e(o)},setup(){const e=s("isMobile");return()=>e.value?a(o,null,null):a(n,null,null)}});export default c;
