f2e11fa32292615f98ecbabdefb530f095ea1ee7
RyanChent <rwqga@qq.com>
2021-08-28 18:04:57 +0800

    fix-snapshot-and-rest-form-and-uirender

---
+++ assets/index.2253bef5.js
@@ -0,0 +1,1 @@
+import{V as e}from"./index.5c29971e.js";import{d as o,f as n}from"./vendor.0ef042a4.js";const a=o({name:"VideoPage",componentName:"VideoPage",components:{VideoPlayer:o(e)},render:()=>n(e,{wideScreen:!0,webFullScreen:!0,download:!0},null)});export default a;
